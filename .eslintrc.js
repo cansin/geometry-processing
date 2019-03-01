7f49b1d906ccf80e862346d648f436480dc4e73f
Cansın Yıldız <cansinyildiz@gmail.com>
2019-03-02 00:01:33 +0300

    Add the Hello World application for three.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,14 @@ module.exports = {
         browser: true,
         es6: true,
     },
-    extends: ["plugin:prettier/recommended"],
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
     rules: {},
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-function component() {
-    let element = document.createElement("div");
+import * as three from "three";
 
-    element.innerHTML = "CENG 789 - Digital Geometry Processing - Assignment I";
+const scene = new three.Scene();
+const camera = new three.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
+);
 
-    return element;
-}
+const renderer = new three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-document.body.appendChild(component());
+const geometry = new three.BoxGeometry(1, 1, 1);
+const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new three.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = function() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+};
+
+animate();
