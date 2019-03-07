4bdd2a850137fb3ef8166c90129183c89dc191d5
Cansın Yıldız <cansinyildiz@gmail.com>
2019-03-07 18:26:03 +0300

    Further simplify the code.

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import {
     PerspectiveCamera,
     PointLight,
     Scene,
-    TextureLoader,
     WebGLRenderer,
 } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 
 import dragon from "./models/dragon.obj";
-import uvGridSm from "./textures/UV_Grid_Sm.jpg";
 import "./index.less";
 
 let object;
 
-const container = document.createElement("div");
-document.body.appendChild(container);
+// camera
 const camera = new PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
@@ -25,53 +22,34 @@ const camera = new PerspectiveCamera(
     2000,
 );
 camera.position.z = 250;
+camera.add(new PointLight(0xffffff, 0.8));
 
 // scene
 const scene = new Scene();
-const ambientLight = new AmbientLight(0xcccccc, 0.4);
-scene.add(ambientLight);
-const pointLight = new PointLight(0xffffff, 0.8);
-camera.add(pointLight);
+scene.add(new AmbientLight(0xcccccc, 0.4));
 scene.add(camera);
 
 // manager
-const manager = new LoadingManager(() => {
-    object.traverse(child => {
-        if (child.isMesh) child.material.map = texture;
-    });
-    scene.add(object);
-});
-manager.onProgress = (item, loaded, total) => {
-    console.log(item, loaded, total);
-};
-
-// texture
-const textureLoader = new TextureLoader(manager);
-const texture = textureLoader.load(uvGridSm);
+const manager = new LoadingManager(
+    () => {
+        scene.add(object);
+    },
+    (item, loaded, total) => {
+        console.log(item, loaded, total);
+    },
+);
 
 // model
 const loader = new OBJLoader(manager);
-loader.load(
-    dragon,
-    obj => {
-        object = obj;
-    },
-    xhr => {
-        if (xhr.lengthComputable) {
-            const percentComplete = (xhr.loaded / xhr.total) * 100;
-            console.log(
-                "model " + Math.round(percentComplete, 2) + "% downloaded",
-            );
-        }
-    },
-    () => {},
-);
+loader.load(dragon, obj => {
+    object = obj;
+});
 
 //
 const renderer = new WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
 // controls
 const controls = new TrackballControls(camera, renderer.domElement);
