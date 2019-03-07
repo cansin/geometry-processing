9a39038b1f48ed6deeff977cdcf65cbc0761cb88
Cansın Yıldız <cansinyildiz@gmail.com>
2019-03-07 18:13:43 +0300

    Simplify the code.

--- src/index.js
+++ src/index.js
@@ -8,104 +8,86 @@ import {
     WebGLRenderer,
 } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
+
 import dragon from "./models/dragon.obj";
 import uvGridSm from "./textures/UV_Grid_Sm.jpg";
 import "./index.less";
 
-let container;
-let camera, scene, renderer;
-let mouseX = 0,
-    mouseY = 0;
-let windowHalfX = window.innerWidth / 2;
-let windowHalfY = window.innerHeight / 2;
 let object;
 
-init();
+const container = document.createElement("div");
+document.body.appendChild(container);
+const camera = new PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    2000,
+);
+camera.position.z = 250;
 
-animate();
+// scene
+const scene = new Scene();
+const ambientLight = new AmbientLight(0xcccccc, 0.4);
+scene.add(ambientLight);
+const pointLight = new PointLight(0xffffff, 0.8);
+camera.add(pointLight);
+scene.add(camera);
 
-function init() {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-    camera = new PerspectiveCamera(
-        45,
-        window.innerWidth / window.innerHeight,
-        1,
-        2000,
-    );
-    camera.position.z = 250;
-    // scene
-    scene = new Scene();
-    const ambientLight = new AmbientLight(0xcccccc, 0.4);
-    scene.add(ambientLight);
-    const pointLight = new PointLight(0xffffff, 0.8);
-    camera.add(pointLight);
-    scene.add(camera);
-    // manager
-    function loadModel() {
-        object.traverse(function(child) {
-            if (child.isMesh) child.material.map = texture;
-        });
-        object.position.y = -95;
-        scene.add(object);
-    }
-    const manager = new LoadingManager(loadModel);
-    manager.onProgress = function(item, loaded, total) {
-        console.log(item, loaded, total);
-    };
-    // texture
-    const textureLoader = new TextureLoader(manager);
-    const texture = textureLoader.load(uvGridSm);
-    // model
-    function onProgress(xhr) {
+// manager
+const manager = new LoadingManager(() => {
+    object.traverse(child => {
+        if (child.isMesh) child.material.map = texture;
+    });
+    object.position.y = -95;
+    scene.add(object);
+});
+manager.onProgress = (item, loaded, total) => {
+    console.log(item, loaded, total);
+};
+
+// texture
+const textureLoader = new TextureLoader(manager);
+const texture = textureLoader.load(uvGridSm);
+
+// model
+const loader = new OBJLoader(manager);
+loader.load(
+    dragon,
+    obj => {
+        object = obj;
+    },
+    xhr => {
         if (xhr.lengthComputable) {
             const percentComplete = (xhr.loaded / xhr.total) * 100;
             console.log(
                 "model " + Math.round(percentComplete, 2) + "% downloaded",
             );
         }
-    }
-    function onError() {}
-    const loader = new OBJLoader(manager);
-    loader.load(
-        dragon,
-        function(obj) {
-            object = obj;
-        },
-        onProgress,
-        onError,
-    );
-    //
-    renderer = new WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-    document.addEventListener("mousemove", onDocumentMouseMove, false);
-    //
-    window.addEventListener("resize", onWindowResize, false);
-}
+    },
+    () => {},
+);
 
-function onWindowResize() {
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
+//
+const renderer = new WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
+
+// controls
+const controls = new TrackballControls(camera, renderer.domElement);
+
+//
+window.addEventListener("resize", () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-}
+});
 
-function onDocumentMouseMove(event) {
-    mouseX = (event.clientX - windowHalfX) / 2;
-    mouseY = (event.clientY - windowHalfY) / 2;
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-    render();
-}
-
-function render() {
-    camera.position.x += (mouseX - camera.position.x) * 0.05;
-    camera.position.y += (-mouseY - camera.position.y) * 0.05;
-    camera.lookAt(scene.position);
+const animate = () => {
+    window.requestAnimationFrame(animate);
+    controls.update();
     renderer.render(scene, camera);
-}
+};
+
+window.requestAnimationFrame(animate);
