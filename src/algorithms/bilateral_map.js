827f63504e72f63bbb3cc324c4cdbde8d98ef371
Cansın Yıldız <cansinyildiz@gmail.com>
2019-03-09 22:24:53 +0300

    Fix the Q type bug for Bilateral Map.

--- src/algorithms/bilateral_map.js
+++ src/algorithms/bilateral_map.js
@@ -1,7 +1,7 @@
 import { findGeodesicDistance } from "./geodesic";
 
 export function findBilateralMap(graph, source, target) {
-    const { path } = findGeodesicDistance(graph, source, target, false);
+    const { path } = findGeodesicDistance(graph, source, target);
 
     return {
         path,
